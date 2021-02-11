49d0476817f54f6df4692403b2679e8e8970bbb4
Александр Корнев <sanyasalsk2@gmail.com>
2021-02-11 15:46:22 +0300

    add swiper, accordeon, tabs

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const tbsList = document.querySelectorAll('.step-button')
+  const burger = document.querySelector('.burger')
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      bulletClass: 'point',
+      bulletActiveClass: 'point-active'
+    },
+
+    autoplay: {
+      delay: 5000,
+    },
+  });
+  $(function () {
+    $("#accordion").accordion({
+      active: false,
+      collapsible: true
+    });
+  });
+
+  tbsList.forEach(el => {
+    el.addEventListener('click', function (event) {
+      tbsList.forEach(el => el.classList.remove('is-active-tbl'))
+      const path = event.currentTarget.dataset.path
+      document.querySelector(`[data-path="${path}"]`).classList.add('is-active-tbl')
+      document.querySelectorAll('.step-animation-list').forEach(el => {
+        el.classList.remove('is-active')
+      })
+      document.querySelector(`[data-target="${path}"]`).classList.add('is-active')
+    })
+  })
+})
